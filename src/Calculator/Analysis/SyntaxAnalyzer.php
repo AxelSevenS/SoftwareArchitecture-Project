75d2868e99e792fd33e6f2fe99699fcef4e113cf
AxelSeven <spaghettiandmeatballs61@gmail.com>
2024-11-08 21:58:01 +0100

    refactor(analyzers): Simplify symbol tokenization logic in SyntaxAnalyzer by consolidating functions and improving readability

--- src/Calculator/Analysis/SyntaxAnalyzer.php
+++ src/Calculator/Analysis/SyntaxAnalyzer.php
@@ -30,41 +30,37 @@ abstract class SyntaxAnalyzer {
 		return $this->tokenize_symbols($symbols, $tokens);
 	}
 
-	private function detach_symbols_by_token(array& $symbol_parts, string& $token): array {
-		return array_reduce($symbol_parts, function ($detached_parts, $part) use ($token) {
-			if (strpos($part, $token) !== false) {
-				$escapedToken = preg_quote($token, '/');
-
-				$split_parts = preg_split(
-					"/($escapedToken)/",
-					$part,
-					-1,
-					PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY
-				);
-				$split_parts = array_map('trim', $split_parts);
-
-				$split_parts = array_filter(
-					$split_parts,
-					fn($value) => !is_null($value) && $value !== ''
-				);
-
-				$detached_parts = array_merge($detached_parts, $split_parts);
-			} else {
-				$detached_parts[] = $part;
-			}
-			return $detached_parts;
-		}, []);
-	}
-
 
 	private function tokenize_symbols(array& $symbols, array& $tokens): array {
-		return array_reduce($symbols, function ($tokenized_symbols, $symbol) use ($tokens) {
-			$symbol_parts = [$symbol];
-			foreach ($tokens as $token) {
-				$symbol_parts = $this->detach_symbols_by_token($symbol_parts, $token);
-			}
-			return array_merge($tokenized_symbols, $symbol_parts);
-		}, []);
+		usort($tokens, fn($a, $b) => strlen($b) - strlen($a));
+		$escapedTokens = '~(' . implode('|', array_map('preg_quote', $tokens)) . ')~';
+
+		return array_reduce(
+			$symbols,
+			function ($tokenized_symbols, $symbol) use ($escapedTokens) {
+				return array_merge(
+					$tokenized_symbols,
+					array_reduce([$symbol], function ($detached_parts, $part) use ($escapedTokens) {
+
+						$split_parts = preg_split(
+							$escapedTokens,
+							$part,
+							-1,
+							PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY
+						);
+						$split_parts = array_map('trim', $split_parts);
+
+						$split_parts = array_filter(
+							$split_parts,
+							fn($value) => !is_null($value) && $value !== ''
+						);
+
+						$detached_parts = array_merge($detached_parts, $split_parts);
+						return $detached_parts;
+					}, [])
+				);
+			}, []
+		);
 	}
 
 	abstract protected function _get_tokens(): array;
